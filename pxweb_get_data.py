3005bfa94adc37dd37ed39e874ffde715fb0d026
trananhthong <32213097+trananhthong@users.noreply.github.com>
2019-10-03 11:30:51 +0300

    Added functions to download data directly to dataframes

--- pxweb_get_data.py
+++ pxweb_get_data.py
@@ -2,6 +2,8 @@ import os
 import requests
 import time
 import json
+import io
+import pandas as pd
 
 ##NOTE: Table 9_koko access is forbidden from the API for some reasons.
 
@@ -37,6 +39,7 @@ def fetch_paavo(destination_directory):
     levels = []
     response = requests.post('http://pxnet2.stat.fi/PXWeb/api/v1/en/Postinumeroalueittainen_avoin_tieto/')
     response_texts = json.loads(response.text)
+
     for response_text in response_texts:
         levels.append(str(response_text['id']))
 
@@ -54,3 +57,50 @@ def fetch_paavo(destination_directory):
             url = paavo_url(level, id)
             file_name = name + '.csv'
             fetch_csv(url, paavo_directory, file_name)
+
+# Download a table from PXweb API to a DataFrame
+def fetch_dataframe(url):
+    response = requests.post(url, json={"query": [], "response": {"format": "csv"}}, stream=True, allow_redirects=True)
+
+    if response.status_code == 200:
+        byte_data = io.BytesIO(response.content)
+        df = pd.read_csv(byte_data, sep=',', encoding='iso-8859-1')
+        print('Downloaded data from ' + url)
+        return df
+    else:
+        print('Could not download from ' + url)
+        print('HTTP/1.1 ' + str(response.status_code))
+        return pd.DataFrame()
+
+    time.sleep(1)
+ 
+# Download the whole paavo directory to a dictionary with names as keys and dataframes as values
+def paavo_data():
+    data = {}
+    # Getting levels from paavo database
+    levels = []
+    response = requests.post('http://pxnet2.stat.fi/PXWeb/api/v1/en/Postinumeroalueittainen_avoin_tieto/')
+    response_texts = json.loads(response.text)
+
+    for response_text in response_texts:
+        levels.append(str(response_text['id']))
+
+    for level in levels:
+        response = requests.post(
+            'http://pxnet2.stat.fi/PXWeb/api/v1/en/Postinumeroalueittainen_avoin_tieto/' + str(level))
+        response_texts = json.loads(response.text)
+        table_data = {}
+
+        for response_text in response_texts:
+            table_data[response_text['id']] = str(response_text['text']).split('. ')[-1].replace("'", "").replace(" ", "_")
+
+        for (id, name) in table_data.items():
+            url = paavo_url(level, id)
+            df = fetch_dataframe(url)
+
+            if not df.empty:
+                data[name] = df
+
+            time.sleep(1)
+
+    return data

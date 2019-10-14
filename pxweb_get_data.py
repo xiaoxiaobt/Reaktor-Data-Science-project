583a8070e4ebb7f99b690dfd6c4995707dbf43f2
trananhthong <32213097+trananhthong@users.noreply.github.com>
2019-10-14 23:46:36 +0300

    Added json query as a parameter for fetching single table

--- pxweb_get_data.py
+++ pxweb_get_data.py
@@ -5,10 +5,11 @@ import json
 import io
 import pandas as pd
 
-##NOTE: Table 9_koko access is forbidden from the API for some reasons.
+## NOTE: Table 9_koko access is forbidden from the API for some reasons.
 
 # url to the API
 main_paavo_url = 'http://pxnet2.stat.fi/PXWeb/api/v1/en/Postinumeroalueittainen_avoin_tieto/'
+
 
 ## Helper to make url to the paavo API
 def paavo_url(level, table):
@@ -16,8 +17,8 @@ def paavo_url(level, table):
 
 
 # Fetch a single file from PXweb API. File name should end with '.csv'
-def fetch_csv(url, destination_directory, file_name):
-    response = requests.post(url, json = {"query":[],"response":{"format":"csv"}}, stream=True, allow_redirects=True)
+def fetch_csv(url, destination_directory, file_name, query={"query": [], "response": {"format": "csv"}}):
+    response = requests.post(url, json=query, stream=True, allow_redirects=True)
 
     if not os.path.exists(destination_directory):
         os.makedirs(destination_directory)
@@ -33,6 +34,7 @@ def fetch_csv(url, destination_directory, file_name):
 
     time.sleep(1)
 
+
 # Fetch the whole paavo directory
 def fetch_paavo(destination_directory):
     # Getting levels from paavo database
@@ -46,9 +48,9 @@ def fetch_paavo(destination_directory):
     paavo_directory = os.path.join(destination_directory, 'paavo_data')
 
     for level in levels:
-        response = requests.post('http://pxnet2.stat.fi/PXWeb/api/v1/en/Postinumeroalueittainen_avoin_tieto/'+ str(level))
+        response = requests.post('http://pxnet2.stat.fi/PXWeb/api/v1/en/Postinumeroalueittainen_avoin_tieto/' + str(level))
         response_texts = json.loads(response.text)
-        table_data={}
+        table_data = {}
 
         for response_text in response_texts:
             table_data[response_text['id']] = str(response_text['text']).split('. ')[-1].replace("'", "").replace(" ", "_")
@@ -58,9 +60,10 @@ def fetch_paavo(destination_directory):
             file_name = name + '.csv'
             fetch_csv(url, paavo_directory, file_name)
 
+
 # Download a table from PXweb API to a DataFrame
-def fetch_dataframe(url):
-    response = requests.post(url, json={"query": [], "response": {"format": "csv"}}, stream=True, allow_redirects=True)
+def fetch_dataframe(url, query={"query": [], "response": {"format": "csv"}}):
+    response = requests.post(url, json=query, stream=True, allow_redirects=True)
 
     if response.status_code == 200:
         byte_data = io.BytesIO(response.content)
@@ -73,7 +76,8 @@ def fetch_dataframe(url):
         return pd.DataFrame()
 
     time.sleep(1)
- 
+
+
 # Download the whole paavo directory to a dictionary with names as keys and dataframes as values
 def paavo_data():
     data = {}
